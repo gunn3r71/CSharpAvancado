fe0a1ce7408a087b91d6a81f25acef047af8cfd2
Lucas <lucasgunner3@gmail.com>
2021-08-05 19:40:25 -0300

    Serialização do objeto com Json

---
+++ SerializarJSON/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using SerializacaoXML.Entities;
+
+namespace SerializarJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User
+            {
+                Username = "Lucas",
+                Email = "lucas.p.oliveira@outlook.pt",
+                BirthDate = new DateTime(2002, 07, 30)
+            };
+
+            var json = new JsonSerializer();
+            
+            using (var jsonSerializer = new JsonTextWriter(new StreamWriter(@"C:\Users\lucas\Downloads\JsonSave\teste.json")))
+            {
+                json.Serialize(jsonSerializer, user);
+            }
+        }
+    }
+}
