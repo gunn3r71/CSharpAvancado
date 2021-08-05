862d3e7807f985fc481c8d33cfc2a094ebb5f027
Lucas <lucasgunner3@gmail.com>
2021-08-05 19:45:55 -0300

    Deserialização de Json

---
+++ DeserialzacaoJson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using SerializacaoXML.Entities;
+
+namespace DeserialzacaoJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+
+            var json = new JsonSerializer();
+
+            using (var jsonSerializer = new JsonTextReader(new StreamReader(@"C:\Users\lucas\Downloads\JsonSave\teste.json")))
+            {
+                user = json.Deserialize<User>(jsonSerializer);
+            }
+
+            Console.WriteLine(user);
+        }
+    }
+}
